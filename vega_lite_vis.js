f824d53f71d3a7909737f7430c1d2d3b5eade492
zoonmattau <54747987+zoonmattau@users.noreply.github.com>
2022-10-14 10:11:12 +1100

    Add files via upload

---
+++ vega_lite_vis.js
@@ -0,0 +1,12 @@
+var vg_1 = "daily_cost_bar_chart.vg.json";
+
+vegaEmbed("#bar_chart", vg_1).then(function(result) {
+
+}).catch(console.error);
+
+var vg_2 = "wealth_by_country.vg.json";
+
+vegaEmbed("#cholopleth_map", vg_2).then(function(result) {
+
+}).catch(console.error);
+
