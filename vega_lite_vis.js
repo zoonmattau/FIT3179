689ef9746225d00bf3ca517a8e0a4981284874de
zoonmattau <54747987+zoonmattau@users.noreply.github.com>
2022-10-17 21:55:09 +1100

    Add files via upload

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -10,3 +10,27 @@ vegaEmbed("#cholopleth_map", vg_2).then(function(result) {
 
 }).catch(console.error);
 
+var vg_3 = "scatterwealth.vg.json";
+
+vegaEmbed("#scatterplot", vg_3).then(function(result) {
+
+}).catch(console.error);
+
+var vg_4 = "gender.vg.json";
+
+vegaEmbed("#gender_bar", vg_4).then(function(result) {
+
+}).catch(console.error);
+
+var vg_5 = "inherited.vg.json";
+
+vegaEmbed("#inherited_bar", vg_5).then(function(result) {
+
+}).catch(console.error);
+
+var vg_6 = "wealth_by_age.vg.json";
+
+vegaEmbed("#age_hist", vg_6).then(function(result) {
+
+}).catch(console.error);
+
